910648082f83fa1483dd81e70bf67e39ccce0104
ifanzalukhu97 <ivan.zalukhu97@gmail.com>
2019-07-27 00:26:59 +0700

    02-Solution with POJO

--- app/src/main/java/com/ifanjszalukhu/databindinggdk19/MainActivity.java
+++ app/src/main/java/com/ifanjszalukhu/databindinggdk19/MainActivity.java
@@ -9,16 +9,18 @@ import com.ifanjszalukhu.databindinggdk19.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    // TODO 4: Rebuild Project
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
-        // TODO 2: set current movie
+        Movie currentMovie = new Movie(
+                "Raja Singa (2019)",
+                "Simba mengidolakan ayahnya, Raja Mufasa, dan mengambil hati takdir kerajaannya sendiri. Tetapi tidak semua orang di kerajaan itu merayakan kedatangan anaknya yang baru.",
+                "https://image.tmdb.org/t/p/w533_and_h300_bestv2/1TUg5pO1VZ4B0Q1amk3OlXvlpXV.jpg"
+        );
 
-        // TODO 5: set movie variable data binding values
+        binding.setMovie(currentMovie);
     }
 }
--- app/src/main/java/com/ifanjszalukhu/databindinggdk19/Movie.java
+++ app/src/main/java/com/ifanjszalukhu/databindinggdk19/Movie.java
@@ -1,5 +1,37 @@
 package com.ifanjszalukhu.databindinggdk19;
 
-// TODO 1: Create Movie POJO Class
 public class Movie {
+    private String title;
+    private String overview;
+    private String poster;
+
+    public Movie(String title, String overview, String poster) {
+        this.title = title;
+        this.overview = overview;
+        this.poster = poster;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public void setPoster(String poster) {
+        this.poster = poster;
+    }
 }
