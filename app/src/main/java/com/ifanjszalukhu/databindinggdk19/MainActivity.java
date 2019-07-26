09ce189b55507785617080cc3f51ceed8d414797
ifanzalukhu97 <ivan.zalukhu97@gmail.com>
2019-07-26 22:41:14 +0700

    01-Starter Basic

---
+++ app/src/main/java/com/ifanjszalukhu/databindinggdk19/MainActivity.java
@@ -0,0 +1,20 @@
+package com.ifanjszalukhu.databindinggdk19;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+//TODO 5: Rebuild Project
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //TODO 6: Change inflation
+        setContentView(R.layout.activity_main);
+
+        //TODO 7: set variable values data binding
+    }
+}
