4b709376b94fadb883be08c408578b723718d8bd
ifanzalukhu97 <ivan.zalukhu97@gmail.com>
2019-07-28 12:25:04 +0700

    03-Solution Binding Adapter

--- app/src/main/java/com/ifanjszalukhu/databindinggdk19/BindingAdapters.java
+++ app/src/main/java/com/ifanjszalukhu/databindinggdk19/BindingAdapters.java
@@ -1,6 +1,17 @@
 package com.ifanjszalukhu.databindinggdk19;
 
+import android.widget.ImageView;
+
+import androidx.databinding.BindingAdapter;
+
+import com.bumptech.glide.Glide;
+
 public class BindingAdapters {
 
-    //TODO 3: Add Binding Adapter to handle image poster
+    @BindingAdapter("img_poster")
+    public static void setImagePoster(ImageView image, String poster) {
+        Glide.with(image.getContext())
+                .load(poster)
+                .into(image);
+    }
 }
